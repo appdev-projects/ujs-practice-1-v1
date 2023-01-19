1c4ab5d591e06ccba563f65b8fcf7afd4c9cabd8
acuevasd <acuevasd@chicagobooth.edu>
2023-01-19 20:17:40 +0000

    annotate

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  status     :string           default("not_yet_started"), not null
+#  user       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
