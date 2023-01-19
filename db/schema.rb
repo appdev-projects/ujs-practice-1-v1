bc29b74f55ba6c1d2f6b8a324d825c960770c5bb
acuevasd <acuevasd@chicagobooth.edu>
2023-01-19 19:52:06 +0000

    Task and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_19_194659) do
+ActiveRecord::Schema.define(version: 2023_01_19_195124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.string "user", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
