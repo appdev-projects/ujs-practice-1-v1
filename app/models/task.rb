fd88460525f4a97c94e839be32a1cbd217c18fb5
disanchez27edu <disanchez@uchicago.edu>
2023-01-19 22:33:16 +0000

    created task list partial, new task form

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
 
   belongs_to :owner, required: true, class_name: "User", foreign_key: "owner_id", counter_cache: true
 
-  enum status: { pending: "not yet started", wip: "in progress", completed: "completed" } 
+  enum status: { pending: "not yet started", wip: "in progress", completed: "completed" }
 
 end
