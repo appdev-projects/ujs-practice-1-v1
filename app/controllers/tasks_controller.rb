1ed07832d8e07baae34a95660516b0ee1644c607
acuevasd <acuevasd@chicagobooth.edu>
2023-01-20 23:10:08 +0000

    destroy working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -87,9 +87,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
       format.json { head :no_content }
-      format.js do
-        render template: "comments/destroy.js.erb"
-      end
+      format.js
     end
   end
 
