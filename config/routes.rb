ee85483f5ae493898ce164718c936669eb8732c3
dmacinto <dmacinto@gmail.com>
2023-01-21 18:44:41 +0000

    removed photogram elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 # == Route Map
 
 Rails.application.routes.draw do
-  root "photos#index"
+  root "tasks#index"
 
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get ":username/liked" => "photos#liked", as: :liked_photos
-  get ":username/feed" => "users#feed", as: :user_feed
-  get ":username/followers" => "users#followers", as: :user_followers
-  get ":username/following" => "photos#following", as: :photo_following
 
   get "/:username" => "users#show", as: :user
 end
