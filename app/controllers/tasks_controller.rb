0be0f0faccd184ed90d608c589746779843e5ba1
dmacinto <dmacinto@gmail.com>
2023-01-21 21:12:20 +0000

    added move action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
 
+  def move
+    if @task.not_yet_started?
+      @task.in_progress!
+    elsif @task.in_progress?
+      @task.completed!
+    else
+      @task.in_progress!
+    end
+
+    respond_to do |format|
+      format.html { redirect_to task_url, notice: "Task updated"}
+    end
+  end
+
+
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
