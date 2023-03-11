c0cfd2dfa3110988bfcba4e87aa1618168a0f12f
bomoniyi <bomoniyi@chicagobooth.edu>
2023-03-11 06:13:33 +0000

    UJS_updated2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[ show edit update destroy move ]
+
+  def move
+    if @task.not_yet_started?
+      @task.in_progress!
+    elsif @task.in_progress?
+      @task.completed!
+    else
+      @task.in_progress!
+    end
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "Task updated" }
+    end
+  end
 
   # GET /tasks or /tasks.json
   def index
@@ -58,13 +72,14 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.fetch(:task, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def task_params
+    params.fetch(:task, {})
+  end
 end
